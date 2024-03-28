6fd3db90932f84b9c3532eba928e513ec70fdd4b
BigZaddy0 <99842417+BigZaddy0@users.noreply.github.com>
2024-03-28 15:29:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.options("*", (req, res) => {
     res.send();
   });
   
-app.use(express.static('build'))
+
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
